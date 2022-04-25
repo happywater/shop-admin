dbe40a08c553b3f52f0ee41112fb40c65caa8a5d
zhaiqiuqiu <zhaiqiuqiu@wxchina.com>
2022-04-25 21:57:53 +0800

    vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,8 @@
+import { State } from 'vue'
 import { createStore } from 'vuex'
 
 // 创建一个新的 store 实例
-const store = createStore({
+const store = createStore<State>({
   state () {
     return {
       count: 10
