a71f3dda1de81e973a9dc000a05806837398cff8
zhaiqiuqiu <zhaiqiuqiu@wxchina.com>
2022-04-30 21:56:36 +0800

    alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     eslintPlugin()
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  }
 })
